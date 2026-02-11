fd10210deb842af320499398ad9fc6c1cbb4a3a6
Francisco Lago Cervera <Francisolagocervera@gmail.com>
2026-02-11 09:26:59 +0100

    Initial commit: Social network API with publications, messages and users

--- api/controllers/message.js
+++ api/controllers/message.js
@@ -5,3 +5,13 @@ var monment = require('moment');
 var User = require('../models/user');
 var Follow = require('../models/follow');
 var Message = require('../models/message');
+
+function probando(req, res) {
+    res.status(200).send({
+        message: 'Hola mundo desde el controlador de mensajes 😎'
+    });
+}
+
+module.exports = {
+    probando
+}
